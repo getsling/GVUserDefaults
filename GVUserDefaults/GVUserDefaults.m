bcb595d3a18f62f6bd451272838d2b06a6cf60e8
NazgulLee <lijian1994@hotmail.com>
2016-11-23 19:10:15 +0800

    enhanced generateAccessorMethods (#33)

--- GVUserDefaults/GVUserDefaults.m
+++ GVUserDefaults/GVUserDefaults.m
@@ -185,6 +185,10 @@ static void objectSetter(GVUserDefaults *self, SEL _cmd, id object) {
     for (int i = 0; i < count; ++i) {
         objc_property_t property = properties[i];
         const char *name = property_getName(property);
+        if (strcmp(name, "mapping") == 0 || strcmp(name, "userDefaults") == 0) {
+            continue;
+        }
+        
         const char *attributes = property_getAttributes(property);
 
         char *getter = strstr(attributes, ",G");
